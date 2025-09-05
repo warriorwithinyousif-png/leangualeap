d093ff7c8ed94d7f496f5b1913014a0693a9b023
yousifnetayad <yousifnetayad@gmail.com>
2025-09-05 09:13:14 +0000

    try2

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,28 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
